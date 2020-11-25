a9cb31fb1822a641402bfa4575ba87db01249864
olafkotur <olafkotur97@gmail.com>
2020-11-25 19:18:39 +0000

    Fixed user agent related issues

--- src/components/WebView/WebView.tsx
+++ src/components/WebView/WebView.tsx
@@ -1,3 +1,4 @@
+import { BrowserView } from 'electron';
 import React from 'react';
 
 interface IProps {
@@ -8,6 +9,8 @@ export default class WebView extends React.Component<IProps> {
   render() {
     return (
       <webview
+        // DANGER: ideally this shouldn't have to be case but here we are
+        useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36"
         className="vh-100"
         src={this.props.url}
       />
