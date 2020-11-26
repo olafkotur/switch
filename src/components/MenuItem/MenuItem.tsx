fa5bfcc5ee8d0c93c1150b7eba2f2c96141ce1f1
olafkotur <olafkotur97@gmail.com>
2020-11-26 13:13:04 +0000

    Added basic pop over for menu items

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ButtonBase } from '@material-ui/core';
-import { Image } from '@material-ui/icons';
+import { ButtonBase, IconButton, Paper, Slide } from '@material-ui/core';
+import { Image, Refresh, Home, ArrowBack, ArrowForward } from '@material-ui/icons';
 import { TPages } from '../../pages/Dashboard/Dashboard';
 import { IMenuItem } from '../../typings/d';
 import './menuItem.css';
@@ -29,26 +29,54 @@ export default class MenuItem extends React.Component<IProps, IState> {
   }
 
   protected handleContextMenu() {
+    console.log('hello');
     this.setState({ contextMenu: !this.state.contextMenu });
   }
 
   render() {
     return (
-      <>
+      <div className="row">
         <ButtonBase
           className={`menu-item mt-2 ${this.props.page === 'web' && this.props.focused ? 'menu-item-selected' : ''}`}
           onClick={() => this.props.handleClick('web', this.props.data)}
           onContextMenu={this.handleContextMenu}
+          onBlur={this.handleContextMenu}
+
         >
           {this.props.data.icon
             ? <img className="menu-item-image" src={this.props.data.icon} />
             : <Image className="menu-item-image" color="secondary" />
           }
         </ButtonBase>
-        {this.state.contextMenu &&
-          <div className="position-absolute" style={{width: 100, height: 100, background: 'red' }}>Hello World</div>
-        }
-      </>
+          <Slide
+            in={this.state.contextMenu}
+            direction="right"
+          >
+            <Paper className="d-flex flex-row position-absolute mt-1 bg-primary" elevation={10}>
+              {this.props.data.icon
+                ? <img className="menu-item-image align-self-center ml-1" src={this.props.data.icon} />
+                : <Image className="menu-item-image align-self-center ml-1" color="secondary" />
+              }
+
+              <IconButton onClick={this.handleContextMenu}>
+                <Home fontSize="small" className="text-white-50" />
+              </IconButton>
+
+              <IconButton onClick={this.handleContextMenu}>
+                <Refresh fontSize="small" className="text-white-50" />
+              </IconButton>
+
+              <IconButton onClick={this.handleContextMenu}>
+                <ArrowBack fontSize="small" className="text-white-50" />
+              </IconButton>
+
+              <IconButton onClick={this.handleContextMenu}>
+                <ArrowForward fontSize="small" className="text-white-50" />
+              </IconButton>
+
+            </Paper>
+          </Slide>
+      </div>
     );
   }
 }
