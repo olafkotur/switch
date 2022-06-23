be94e7146e06d9350489e68a44fbf3cb143b5e30
olafkotur <olafkotur97@gmail.com>
2022-06-23 15:25:14 +0100

    Added footer and app version to settings page

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,4 +1,9 @@
+// @ts-ignore
+import { version } from '../../package.json'
+
 export const config = {
+  appVersion: version,
+  projectRepo: 'https://github.com/olafkotur/switch',
   apiUrl: 'http://localhost:8080',
   defaultError: 'Unexpected error occurred',
 }
--- app/src/icons.ts
+++ app/src/icons.ts
@@ -1,3 +1,3 @@
-const d = require('../assets/default-avatar.png')
 export const ICON = require('../assets/switch-icon.png')
-export const DEFAULT_AVATAR = d
+export const DEFAULT_AVATAR = require('../assets/default-avatar.png')
+export const GITHUB_ICON = require('../assets/suggestions/github.png')
