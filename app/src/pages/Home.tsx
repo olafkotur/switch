5090bcfad3b6c3f952a5dc026f80f5c24812602f
olafkotur <olafkotur97@gmail.com>
2022-12-20 21:52:11 +0000

    Added switch header

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -3,18 +3,26 @@ import styled from 'styled-components';
 import { SIDE_BAR_WIDTH_PX } from '../../../common/const';
 import { SearchBar } from '../components';
 
+const switchIcon = require('../../assets/switch-header.svg');
+
 const HomeContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   width: calc(100vw - ${SIDE_BAR_WIDTH_PX}px);
   padding: 0 0 0 ${SIDE_BAR_WIDTH_PX}px;
   background: ${(props) => props.theme.backgroundColor.primary};
 `;
 
+const Header = styled.img`
+  margin-bottom: 75px;
+`;
+
 export const HomePage = (): ReactElement => {
   return (
     <HomeContainer>
+      <Header src={switchIcon} />
       <SearchBar />
     </HomeContainer>
   );
