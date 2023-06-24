5bd1429f42eda0d548ee1da7ed01c191bc2ff96d
olafkotur <olafkotur97@gmail.com>
2023-06-24 20:48:37 +0100

    Removed site clunk

--- site/src/components/Common.tsx
+++ site/src/components/Common.tsx
@@ -5,34 +5,8 @@ export const Spacer = styled.div<{ horizontal?: number; vertical?: number }>`
     `${props.vertical ?? 0}px ${props.horizontal ?? 0}px ${props.vertical ?? 0}px ${props.horizontal ?? 0}px`};
 `;
 
-export const CenterContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%:
-`;
-
 export const RowContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-`;
-
-export const ColumnContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const FlexEndContainer = styled(RowContainer)`
-  justify-content: flex-end;
-`;
-
-export const SpaceBetweenContainer = styled(RowContainer)`
-  width: 100%;
-  justify-content: space-between;
-`;
-
-export const RelativeContainer = styled.div`
-  position: relative;
 `;
